5e408045c05e0b53b63176d21d755683933245b9
hhp1614 <hhp1614@qq.com>
2022-04-25 10:36:20 +0800

    fix: 请求地址使用相对路径

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ export class Api {
    * 获取公司数据
    */
   static async getCompanies() {
-    const res = await fetch('/companies.txt');
+    const res = await fetch('companies.txt');
     const text = await res.text();
     const lines = text.split('\n').filter(v => !!v.trim()).map(v_1 => {
       const [title, address] = v_1.trim().replace(/ +/, ' ').split(' ');
